92d8916dea58f7fc4d8a913739525c1b8829fbb2
vikash kumar singh <53987510+vikash-Kumar-Singh9@users.noreply.github.com>
2021-01-01 18:16:28 +0530

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <h1> button event</h1>
+  <v-btn
+  elevation="2"
+  active-class
+  rounded
+>click me</v-btn>
+</div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+  }
+</script>
