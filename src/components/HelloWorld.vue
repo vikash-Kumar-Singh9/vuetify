fa25635ac963f074fcbd9bbca85ad48d9783e91b
vikash kumar singh <53987510+vikash-Kumar-Singh9@users.noreply.github.com>
2021-01-01 18:34:33 +0530

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,54 @@
 <template>
-<div>
-  <h1> button event</h1>
-  <v-btn
-  elevation="2"
-  active-class
-  rounded
->click me</v-btn>
-</div>
+  <div class="text-center">
+    <v-dialog
+      v-model="dialog"
+      width="500"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="red lighten-2"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Click Me
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title class="headline grey lighten-2">
+          messege print
+        </v-card-title>
+
+        <v-card-text>
+          click to messege alert
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            text
+            @click="dialog = false"
+          >
+            I accept
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
   export default {
     name: 'HelloWorld',
+     data () {
+      return {
+        dialog: false,
+      }
+    },
 
   }
 </script>
